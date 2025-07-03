ee186a033afc1ebb1cbda2868f2fcd77cac9dba8
Vlad Mazurenko <vladmazurenkog@gmail.com>
2025-07-03 12:34:39 +0300

    Add Bundle-analyzer: `rollup-plugin-visualizer`
    `rollup-plugin-visualizer` has best compatibility with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import react from '@vitejs/plugin-react';
 import * as path from 'node:path';
 import { defineConfig } from 'vitest/config';
 import packageJson from './package.json' with { type: 'json' };
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
+   plugins: [react(), visualizer({ open: true })],
 
    server: {
       open: true,
