7e3b5d5ea58c318afc5f1d8fa45d63f97ee8d826
Vlad Mazurenko <vladmazurenkog@gmail.com>
2025-07-09 14:17:47 +0300

    Lazy load `Audio` component

--- src/components/TrackList/List/Track/Track.tsx
+++ src/components/TrackList/List/Track/Track.tsx
@@ -1,26 +1,24 @@
-import { useAppSelector } from '@/app/hooks';
-import { selectTrackById } from '@/features/trackList/trackListSelectors';
 import styles from './Track.module.scss';
 import CoverImage from './CoverImage/CoverImage';
 import TrackInfo from './TrackInfo/TrackInfo';
-import Audio from '@/components/ui/Audio/Audio';
 import TrackBtns from './TrackBtns/TrackBtns';
 import type { TrackIdT } from '@/features/trackList/schema';
+import React, { Suspense } from 'react';
+import Spinner from '@/components/ui/Spinner/Spinner';
+const Audio = React.lazy(() => import('@/components/ui/Audio/Audio'));
 
 type TrackProps = {
    id: TrackIdT;
 };
 
 export default function Track({ id }: TrackProps) {
-   const track = useAppSelector((state) => selectTrackById(state, id));
-
-   if (!track) return null;
-
    return (
       <li className={styles.track} id={id} data-testid={`track-item-${id}`}>
          <CoverImage id={id} />
          <TrackInfo id={id} />
-         <Audio id={id} />
+         <Suspense fallback={<Spinner />}>
+            <Audio id={id} />
+         </Suspense>
          <TrackBtns id={id} />
       </li>
    );
