6a0f176a8c0de09fd02809ae1e3fe5e42bb670c8
Vlad Mazurenko <vladmazurenkog@gmail.com>
2025-07-10 12:21:39 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
    plugins: [react(), visualizer({ open: true })],
 
    server: {
