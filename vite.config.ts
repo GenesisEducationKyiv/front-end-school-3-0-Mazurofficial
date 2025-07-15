cdb02a5b339fd3acd9fe69ffd1b8faf09bfdab32
Vlad Mazurenko <vladmazurenkog@gmail.com>
2025-07-15 17:24:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,6 @@ import packageJson from './package.json' with { type: 'json' };
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname =
-   typeof __dirname !== 'undefined'
-      ? __dirname
-      : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -41,32 +35,7 @@ export default defineConfig({
       globals: true,
       watch: false,
       setupFiles: ['./src/setupTests.ts'],
-      projects: [
-         {
-            extends: true,
-            plugins: [
-               // The plugin will run tests for the stories defined in your Storybook config
-               // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-               storybookTest({
-                  configDir: path.join(dirname, '.storybook'),
-               }),
-            ],
-            test: {
-               name: 'storybook',
-               browser: {
-                  enabled: true,
-                  headless: true,
-                  provider: 'playwright',
-                  instances: [
-                     {
-                        browser: 'chromium',
-                     },
-                  ],
-               },
-               setupFiles: ['.storybook/vitest.setup.ts'],
-            },
-         },
-      ],
+      include: ['src/tests/**/**/*.test.{ts,tsx}'],
    },
    build: {
       sourcemap: true,
