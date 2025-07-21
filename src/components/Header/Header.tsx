336ced3fcb95c2307e2256d0b6cbe50e0abe8405
Vlad Mazurenko <vladmazurenkog@gmail.com>
2025-07-21 15:41:32 +0300

    Test media formats

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import AddTrackBtn from './AddTrackBtn/AddTrackBtn';
 import styles from './Header.module.scss';
 import logo from '@/assets/logo_tunee.svg';
+import logoMobile from '@/assets/logo_tunee_mobile.svg';
 
 export default function Header() {
    return (
       <header className={styles.header}>
          <a href="/">
-            <img src={logo} alt="logo" />
+            <img
+               src={logo}
+               srcSet={`${logoMobile} 480w, ${logo} 800w`}
+               sizes="(max-width: 480px) 138px, 208px"
+               alt="logo"
+            />
          </a>
          <AddTrackBtn />
       </header>
